5f580ee0ddc4bf3a8d1c790c218a7b1620aba08f
abaart <abaart@gmail.com>
2016-05-09 00:43:01 +0200

    test

--- FlaskKasadaka/FlaskKasadaka/__init__.py
+++ FlaskKasadaka/FlaskKasadaka/__init__.py
@@ -361,7 +361,7 @@ def concatenateWavs(messages):
     command.append(path)
     #command.extend(['-r','8k','-c','1','-e','signed-integer',path])
     subprocess.call(command)
-    flash(" ".join(messages))
+    flash(" ".join(command))
     return path
 
 def generateReminderMessage(userURI):
