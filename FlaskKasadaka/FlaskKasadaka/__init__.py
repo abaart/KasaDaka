e14b833b811622975a9c5b4131bc0a4fdcfdcd4f
Andre Baart <abaart@gmail.com>
2016-06-15 22:26:52 +0200

    .wav file recognition regex update

--- FlaskKasadaka/FlaskKasadaka/__init__.py
+++ FlaskKasadaka/FlaskKasadaka/__init__.py
@@ -218,7 +218,7 @@ def adminAudioHome():
     pythonFiles.extend(glob.glob(config.pythonFilesDir+'templates/*.vxml'))
     pythonFiles.extend(glob.glob(config.pythonFilesDir+'templates/admin/*.html'))
     waveFilesInterface = []
-    wavFilePattern = re.compile("""([^\s\\/+"']+\.wav)""",re.I)
+    wavFilePattern = re.compile("""([^\s\\/+"'\*]+\.wav)""",re.I)
     for pythonFile in pythonFiles:
         text = open(pythonFile).read()
         for match in wavFilePattern.findall(text):
