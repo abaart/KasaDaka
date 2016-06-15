06bda5611c2c9d025b66766b0bc1c9f41071125e
Andre Baart <abaart@gmail.com>
2016-06-15 14:17:09 +0200

    primitive testing of recording in admin interface

--- FlaskKasadaka/FlaskKasadaka/__init__.py
+++ FlaskKasadaka/FlaskKasadaka/__init__.py
@@ -572,6 +572,9 @@ def errorVXML(language="en"):
         messages = [lang.audioInterfaceURL + 'error.wav'],
             redirect = 'error.vxml')
 
+@app.route('/static/<path:path>')
+def send_static(path):
+        return send_from_directory('static', path)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",debug=config.debug)
