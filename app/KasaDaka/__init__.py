2077a8d866520142d4db3dc3d1e75f3e8720d24a
abaart <abaart@gmail.com>
2016-10-07 11:30:14 +0200

    changed apache2 config to reflect new location of Flask files

--- app/KasaDaka/__init__.py
+++ app/KasaDaka/__init__.py
@@ -120,4 +120,4 @@ def send_static(path):
 
 #if __name__ == '__main__':
 #    app.run(host="0.0.0.0",debug=app.config['DEBUG'])
-app.run(host="0.0.0.0",debug=config.DEBUG, use_reloader=config.USE_RELOADER)
+#app.run(host="0.0.0.0",debug=config.DEBUG, use_reloader=config.USE_RELOADER)
